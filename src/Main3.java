716b303072eeb77bd2598968981a1789267303ca
chaiheung <chaiheung@naver.com>
2024-02-19 12:41:17 +0900

    첫 자바 코드 연습

---
+++ src/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("alpha");
+        System.out.println("beta");
+        System.out.println("gamma");
+        System.out.println("delta");
+        System.out.println("epsilon");
+    }
+}
