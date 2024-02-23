43cf9a8744277aa435f48d72c4b48d9da30acc49
chaiheung <chaiheung@naver.com>
2024-02-23 15:11:18 +0900

    C05Switch

---
+++ src/ch04/lecture/p2switch/C05Switch.java
@@ -0,0 +1,23 @@
+package ch04.lecture.p2switch;
+
+public class C05Switch {
+    public static void main(String[] args) {
+        String city = "인천";
+
+        switch (city) {
+            case "서울":
+            case "인천":
+                System.out.println("수도권");
+                break;
+
+            case "광주":
+            case "전주":
+                System.out.println("호남");
+                break;
+
+            case "대구":
+            case "부산":
+                System.out.println("영남");
+        }
+    }
+}
