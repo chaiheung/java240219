fe382f9bed0beaf84a69a4e7f880afbc13fdc7e3
chaiheung <chaiheung@naver.com>
2024-02-26 15:14:47 +0900

    p4For>C04DoWhile

---
+++ src/ch04/lecture/p3while/C04DoWhile.java
@@ -0,0 +1,15 @@
+package ch04.lecture.p3while;
+
+public class C04DoWhile {
+    public static void main(String[] args) {
+
+        System.out.println("statement 1");
+
+        boolean b = false;
+
+        while (b) {
+            System.out.println("statement 2");
+        }
+        System.out.println("statement 3");
+    }
+}
