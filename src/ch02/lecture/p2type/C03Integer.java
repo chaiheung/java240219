061c5c68d8dfeb0ff0e58b7dd4505b6721084b47
chaiheung <chaiheung@naver.com>
2024-02-26 09:29:58 +0900

    C01While

---
+++ src/ch02/lecture/p2type/C03Integer.java
@@ -0,0 +1,12 @@
+package ch02.lecture.p2type;
+
+public class C03Integer {
+    public static void main(String[] args) {
+        // 정수 표현 방법
+        int a = 32;
+        int b = 298282;
+        // 2진법: 0과 1로만 작성
+        // 8진법: 8개의 수로만 작성
+        // 16진법: 0~9, A~F, 총 16개의 수로 작성
+    }
+}
---
+++ src/ch02/lecture/p2type/C16Scanner.java
@@ -0,0 +1,14 @@
+package ch02.lecture.p2type;
+
+import java.util.Scanner;
+
+public class C16Scanner {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine(); // 키보드 입력 받음
+        System.out.println("line =" + line);
+
+    }
+}
