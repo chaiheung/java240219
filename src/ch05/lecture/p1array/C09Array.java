975efbe90292e969cfaaca628bccbe53159341f3
chaiheung <chaiheung@naver.com>
2024-02-27 14:25:14 +0900

    ch05>p1array>C09Array

---
+++ src/ch05/lecture/p1array/C09Array.java
@@ -0,0 +1,16 @@
+package ch05.lecture.p1array;
+
+public class C09Array {
+    public static void main(String[] args) {
+        int[] arr1;
+        arr1 = new int[]{5, 3};
+
+        int[] arr2 = {6, 5};
+
+        // 배열의 선언과 할당 동시에 가능
+        int[] arr3 = {8, 3, 2};
+
+        // 생략 불가
+        var arr4 = new int[]{10, 11, 12}
+    }
+}
