48263ffc35362d6fd04e181d9d4aa9b61f56fddb
chaiheung <chaiheung@naver.com>
2024-02-29 15:38:28 +0900

    ch05>p2string>C06ToUpperCase

---
+++ src/ch05/lecture/p2string/C06ToUpperCase.java
@@ -0,0 +1,13 @@
+package ch05.lecture.p2string;
+
+public class C06ToUpperCase {
+    public static void main(String[] args) {
+        String s1 = "my name is son";
+        String s2 = s1.toUpperCase();
+        String s3 = s2.toLowerCase();
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
