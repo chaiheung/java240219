3dd20c6506e9406d14c217a7c6831bfafd58367c
chaiheung <chaiheung@naver.com>
2024-03-07 12:36:26 +0900

    ch06.exercise.Exercise17

---
+++ src/ch06/exercise/Exercise17/Printer.java
@@ -0,0 +1,19 @@
+package ch06.exercise.Exercise17;
+
+public class Printer {
+    static void println(int i) {
+        System.out.println(i);
+    }
+
+    static void println(boolean b) {
+        System.out.println(b);
+    }
+
+    static void println(double v) {
+        System.out.println(v);
+    }
+
+    static void println(String s) {
+        System.out.println(s);
+    }
+}
