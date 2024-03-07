bfd586914f91bc8c9102b570e1f5bc0e981396b8
chaiheung <chaiheung@naver.com>
2024-03-07 12:36:06 +0900

    ch06.exercise.Exercise14

---
+++ src/ch06/exercise/Exercise14/Member.java
@@ -0,0 +1,14 @@
+package ch06.exercise.Exercise14;
+
+public class Member {
+    // 이름, 아이디, 패스워드, 나이
+    String name;
+    String id;
+    String pw;
+    int age;
+
+    public Member(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
