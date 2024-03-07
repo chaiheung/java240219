d8ec5667045a447b92221807996baa9602a74ee4
chaiheung <chaiheung@naver.com>
2024-03-07 09:41:14 +0900

    ch06.exercise.Exercise13

--- src/ch06/exercise/Exercise13/Member.java
+++ src/ch06/exercise/Exercise13/Member.java
@@ -2,8 +2,45 @@ package ch06.exercise.Exercise13;
 
 public class Member {
     // 이름, 아이디, 패스워드, 나이
-    String name;
-    String id;
-    String pw;
-    int age;
+    // String name;
+    // String id;
+    // String pw;
+    // int age;
+
+    private String name;
+    private String id;
+    private String pw;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
