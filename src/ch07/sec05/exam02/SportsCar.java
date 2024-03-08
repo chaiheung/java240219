840466b6400db66e87655c2288055c4716c5c503
chaiheung <chaiheung@naver.com>
2024-03-08 15:27:12 +0900

    ch07.sec05

---
+++ src/ch07/sec05/exam02/SportsCar.java
@@ -0,0 +1,13 @@
+package ch07.sec05.exam02;
+
+public class SportsCar extends Car {
+    public void speedUp() {
+        speed += 10;
+    }
+
+//    @Override
+//    public void stop(){
+//        System.out.println("멈춤");
+//        speed = 0;
+//    }
+}
