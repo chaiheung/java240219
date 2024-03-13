6a80688f1a0f55bbcdb457d86570ca011bdc105e
chaiheung <chaiheung@naver.com>
2024-03-13 14:36:03 +0900

    ch16.exercise/EX06~08

---
+++ src/ch16/exercise/Exercise08/Example.java
@@ -0,0 +1,25 @@
+package ch16.exercise.Exercise08;
+
+public class Example {
+    private static Student[] students = {
+            new Student("홍길동", 90, 96),
+            new Student("신용권", 95, 93)
+    };
+
+    private static double avg(Function<Student> function) {
+        double sum = 0.0;
+        for (Student student : students) {
+            // sum += student.getEnglishScore();
+            sum += function.apply(student);
+        }
+        return sum / students.length;
+    }
+
+    public static void main(String[] args) {
+        double englishAvg = avg(s -> s.getEnglishScore());
+        System.out.println("englishAvg = " + englishAvg);
+
+        double mathAvg = avg(s -> s.getMathScore());
+        System.out.println("mathAvg = " + mathAvg);
+    }
+}
---
+++ src/ch16/exercise/Exercise08/Function.java
@@ -0,0 +1,5 @@
+package ch16.exercise.Exercise08;
+
+public interface Function<T> {
+    public double apply(T t);
+}
