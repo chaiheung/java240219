75fb67c7a721543c153ce6ae1654891af3733770
chaiheung <chaiheung@naver.com>
2024-03-13 11:03:42 +0900

    ch16.lecture.p1lambda.C08

---
+++ src/ch16/lecture/p1lambda/C08Lambda.java
@@ -0,0 +1,19 @@
+package ch16.lecture.p1lambda;
+
+public class C08Lambda {
+    public static void main(String[] args) {
+        C08MyInterface obj1 = () -> {
+            System.out.println("C08Lambda.method");
+        };
+        C08MyInterface obj2 = () -> System.out.println("hello");
+    }
+}
+
+// 1. 매개변수가 2개 이상 있고 리턴이 없는 추상 메소드
+// 2. 매개변수가 1개 있고 리턴이 없는 추상 메소드
+// 3. 매개변수가 없고 리턴이 없는 추상 메소드
+@FunctionalInterface
+interface C08MyInterface {
+    void method();
+}
+// 4. 리턴 있는 추상 메소드
