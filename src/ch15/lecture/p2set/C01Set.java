148a18a67f07148c7f303781e9d2f07cbbe3ad00
chaiheung <chaiheung@naver.com>
2024-03-19 12:25:49 +0900

    ch15.lecture.p2set.C01~C02

--- src/ch15/lecture/p2set/C01Set.java
+++ src/ch15/lecture/p2set/C01Set.java
@@ -17,22 +17,28 @@ public class C01Set {
 
         // 원소가 없을 때만 추가
         boolean b1 = set.add("css"); // true
-        boolean b2 = set.add("java"); // false
-        System.out.println("b1 = " + b1);
-        System.out.println("b2 = " + b2);
+        boolean b2 = set.add("java");// false
+        System.out.println("b1 = " + b1); // true
+        System.out.println("b2 = " + b2); // false
         System.out.println(set.size()); // 5
 
         // 검색 contains
         boolean b3 = set.contains("angular"); // false
-        boolean b4 = set.contains("java"); // true
+        boolean b4 = set.contains("java");// true
         System.out.println("b3 = " + b3);
-        System.out.println("b3 = " + b4);
+        System.out.println("b4 = " + b4);
 
         // 지우기 remove
-        set.remove("java"); // true
-        set.remove("css"); // true
+        boolean b5 = set.remove("java");// true
+        boolean b6 = set.remove("css");// true
         System.out.println(set.size()); // 3
-        System.out.println(set.size());
+        boolean b7 = set.remove("jsp");// false
+        System.out.println(set.size()); // 3
+
+        System.out.println("b5 = " + b5);
+        System.out.println("b6 = " + b6);
+        System.out.println("b7 = " + b7);
+
 
     }
 }
