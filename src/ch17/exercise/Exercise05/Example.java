5302717e7e6a600e43b897fd36f6706533d87f5a
chaiheung <chaiheung@naver.com>
2024-03-21 15:49:11 +0900

    ch17.exercise.Exercise05

---
+++ src/ch17/exercise/Exercise05/Example.java
@@ -0,0 +1,12 @@
+package ch17.exercise.Exercise05;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("This is a java book", "Lambda expressions", "Java8 supports lambda expressions");
+        list.stream().filter(s -> s.matches(".*[jJ][aA][vV][aA].*"))
+                .forEach(System.out::println);
+    }
+}
