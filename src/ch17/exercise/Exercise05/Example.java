4afeceb2373f7af8f8314dc68b6dd5ec60e56054
chaiheung <chaiheung@naver.com>
2024-03-21 16:03:19 +0900

    ch17.exercise.Exercise05

--- src/ch17/exercise/Exercise05/Example.java
+++ src/ch17/exercise/Exercise05/Example.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Example {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("This is a java book", "Lambda expressions", "Java8 supports lambda expressions");
-        list.stream().filter(s -> s.matches(".*[jJ][aA][vV][aA].*"))
+        list.stream().filter(s -> s.toLowerCase().contains("java"))
                 .forEach(System.out::println);
     }
 }
