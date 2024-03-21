95b6398ffebef8214d77638e90033980a6e8223a
chaiheung <chaiheung@naver.com>
2024-03-21 16:33:16 +0900

    ch17.lecture.p1stream.C09

---
+++ src/ch17/lecture/p1stream/C09Sorted.java
@@ -0,0 +1,33 @@
+package ch17.lecture.p1stream;
+
+import java.util.List;
+
+public class C09Sorted {
+    public static void main(String[] args) {
+        // sorted : 정렬된 stream 리턴
+        List<Integer> list = List.of(9, 3, 10, 6, 5, 7, 1);
+        list.stream()
+                .sorted()
+                .forEach(System.out::println);
+        System.out.println();
+
+        list.stream()
+                .distinct() // 순서 바뀌어도 됨
+                .sorted() // 순서 바뀌어도 됨
+                .forEach(System.out::println);
+        System.out.println();
+
+
+        List<String> list1 = List.of("java", "css", "react", "html", "vue");
+        list1.stream()
+                .sorted()
+                .map(e -> e.length())
+                .forEach(System.out::println);
+        System.out.println();
+
+        list1.stream()
+                .map(e -> e.length())
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
