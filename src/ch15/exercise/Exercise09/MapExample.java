83e02e8c5befcb15e8396f5f1725eef1bbf6463c
chaiheung <chaiheung@naver.com>
2024-03-21 09:25:03 +0900

    ch15.exercise.Exercise09

--- src/ch15/exercise/Exercise09/MapExample.java
+++ src/ch15/exercise/Exercise09/MapExample.java
@@ -11,22 +11,22 @@ public class MapExample {
         map.put("hong", 86);
         map.put("white", 92);
 
+
         String name = null;
         int maxScore = 0;
-        int totalScore = 0;
+        double totalScore = 0;
 
         Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
         for (Map.Entry<String, Integer> entry : entrySet) {
+            totalScore += entry.getValue();
             if (entry.getValue() > maxScore) {
                 name = entry.getKey();
                 maxScore = entry.getValue();
             }
-            totalScore += entry.getValue();
         }
 
-        int avgScore = totalScore / map.size();
+        double avgScore = totalScore / map.size();
         System.out.println("평균 점수: " + avgScore);
-
         System.out.println("최고 점수: " + maxScore);
         System.out.println("최고 점수를 받은 아이디: " + name);
 
